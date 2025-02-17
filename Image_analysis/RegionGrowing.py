4a18c649f9ceff6c30f72412409fb4c23fb0086b
hpuented <37450580+hpuented@users.noreply.github.com>
2025-02-17 10:03:53 +0100

    Update RegionGrowing.py

--- Image_analysis/RegionGrowing.py
+++ Image_analysis/RegionGrowing.py
@@ -26,36 +26,35 @@ def RegionGrowing(img_norm, list_sem, umbral):
         the condition.
     '''
     
-    coord_sem = list_sem[0] #Coordenada de la semilla.
-    matriz_cr = np.zeros(img_norm.shape) #Matriz de ceros del tamaño de la imagen normalizada.
-    matriz_cr[coord_sem[0]][coord_sem[1]] = 1 #Se establece como 1 la coordenada de la semilla.
-    pix_sem = img_norm[coord_sem[0]][coord_sem[1]] #Nivel de gris de la semilla.
+    coord_sem = list_sem[0] 
+    matriz_cr = np.zeros(img_norm.shape) 
+    matriz_cr[coord_sem[0]][coord_sem[1]] = 1 
+    pix_sem = img_norm[coord_sem[0]][coord_sem[1]] #Grey seed level.
     
-    coord_homog = [] #Lista vacía.
-    coord_homog.append(coord_sem) #Se añaden a la lista las coordenadas de la semilla.
+    coord_homog = [] 
+    coord_homog.append(coord_sem) 
     
-    ya_comparadas = [] #Lista vacía.
-    ya_comparadas.append(coord_sem) #Se añaden a la lista las coordenadas de la semilla.
+    ya_comparadas = [] 
+    ya_comparadas.append(coord_sem)
     
-    #Ejecución de instrucciones mientras se ejecuta la condición. 
     while (len(coord_homog)!= 0):
-        coordenada = coord_homog.pop(0) #Coordenada de la lista que se ha eliminado, es el píxel a centrar.
-        for i in range(coordenada[0]-1, coordenada[0]+2): #Filas.
-            for j in range (coordenada[1]-1, coordenada[1]+2): #Columnas.
-            #Se cogen coordenadas con vecindad a 8 centradas en la semilla.
+        coordenada = coord_homog.pop(0) #Coordinate of the list that has been removed, is the pixel to be centred.
+        for i in range(coordenada[0]-1, coordenada[0]+2): 
+            for j in range (coordenada[1]-1, coordenada[1]+2): 
+            #Coordinates with neighbourhood at 8 centred on the seed are taken.
                 if ((0 <= i <= (img_norm.shape[0]-1)) and (0 <= j <= (img_norm.shape[1]-1))): 
-                    pixel = img_norm[i][j] #Valor de gris de cada pixel para comparar.
-                    c = (i,j) #Coordenada del píxel vecino.
+                    pixel = img_norm[i][j]
+                    c = (i,j)
                 
                     if c not in ya_comparadas:
-                        ya_comparadas.append(c) #Se añade la coordenada a la lista.
+                        ya_comparadas.append(c)
                         if (pixel >= (pix_sem - umbral)) and (pixel <= (pix_sem + umbral)):
-                        #En este if hay 3 condiciones:
-                            #1. Si el píxel pertenece al intervalo.
-                            #2. Si el píxel no está fuera de los límites de la imagen.
-                            #3. Si la coordenada del vecino NO está aún en ya_comparadas.
+                        #In this if there are 3 conditions:
+                            #1. If the pixel belongs to the interval.
+                            #2. If the pixel is not outside the image boundaries.
+                            #3. If the neighbour coordinate is NOT yet in already_compared.
                         
-                            matriz_cr[i][j] = 1 #Se manda a blanco el valor del píxel.
-                            coord_homog.append(c) #Se añade la coordenada a la lista.
+                            matriz_cr[i][j] = 1 
+                            coord_homog.append(c)
                         
     return matriz_cr
