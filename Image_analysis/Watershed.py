6766a5547c4ebc52b1bf1c268f56768d97137e3a
hpuented <37450580+hpuented@users.noreply.github.com>
2025-02-17 10:05:27 +0100

    Update Watershed.py

--- Image_analysis/Watershed.py
+++ Image_analysis/Watershed.py
@@ -9,42 +9,42 @@ from skimage.segmentation import watershed
 from funciones_p2 import *
 from imimposemin import *
 
-def WatershedExerciseP2(imagen_normalizada, imagen_sobel, coord_sem): 
+def WatershedExercise(imagen_normalizada, imagen_sobel, coord_sem): 
     '''
-    Adición de ruido gaussiano.
+    Addition of Gaussian noise.
     
-    Parámetros:
-        imagen_normalizada : matriz de píxeles de la imagen normalizada.
-        imagen_sobel : matriz de pixeles de la imagen con filtro de sobel.
-        coord_sem : lista con las coordenadas de las semillas.
+    Parameters:
+        normalised_image : pixel array of the normalised image.
+        sobel_image : pixel array of the sobel filtered image.
+        coord_sem : list of seed coordinates.
         
-    Devuelve:
-        img_ws1 : matriz de píxeles resultado de la aplicación de Watershed. A la imagen se le ha aplicado la función imimposemin. 
-        img_ws2 : matriz de píxeles resultado de la aplicación de Watershed. La imagen es aquella que presenta filtro de sobel.
+    Returns:
+        img_ws1 : pixel array resulting from the Watershed application. The imimposemin function has been applied to the image. 
+        img_ws2 : matrix of pixels resulting from the application of Watershed. The image is the one with sobel filter.
     
-    Notas:
-        Se crea la matriz de ceros del tamaño de la imagen original. A continuación se pincha en la imagen, se calculan 
-        las coordenadas de las semillas y se llevan a blanco los pixeles que se encuentran en las coordenas de las semillas pero,
-        en este caso, en la matriz de ceros creada.
-        Se aplican los algortimos imimposemin (sobre la imagen de sobel y la binaria) y watershed (sobre la imagen de imimposemin
-        y sobel). Por último, se muestra en pantalla el resultado de la función imimposemin sobre la imagen normalizada y la binaria. 
+    Notes:
+        The array of zeros of the size of the original image is created. The image is then clicked on, the seed coordinates are calculated and the 
+        the coordinates of the seeds are calculated and the pixels that are in the coordinates of the seeds are brought to white,
+        in this case, in the matrix of zeros created.
+        The algorithms imimposemin (on the sobel and binary image) and watershed (on the imimposemin and sobel image) are applied.
+        and sobel). Finally, the result of the imimposemin function on the normalised and binary image is displayed on the screen. 
     '''
 
-    img_binaria = np.zeros(imagen_normalizada.shape) #Matriz de ceros del tamaño de la imagen de entrada.
+    img_binaria = np.zeros(imagen_normalizada.shape)
 
     for elem in range (len(coord_sem)):
-        coord = coord_sem[elem] #Coordenada i de la lista
-        fila = coord[0] #Fila
-        col = coord[1] #Columna
-        img_binaria[fila][col] = 1 #Se establece como 1 la coordenada de la semilla.
+        coord = coord_sem[elem]
+        fila = coord[0] 
+        col = coord[1]
+        img_binaria[fila][col] = 1 #The seed coordinate is set to 1.
         
-    min_loc = imimposemin(imagen_sobel, img_binaria) #Mínimos locales 
-    img_ws1 = watershed(min_loc) #Watershed con imimposemin
-    img_ws2 = watershed(imagen_sobel) #Watershed sin imimposemin
+    min_loc = imimposemin(imagen_sobel, img_binaria) #Local minima
+    img_ws1 = watershed(min_loc)
+    img_ws2 = watershed(imagen_sobel)
     
-    min_norm = imimposemin(imagen_normalizada, img_binaria) #Mínimos locales 
+    min_norm = imimposemin(imagen_normalizada, img_binaria)
     plt.figure(figsize=[8,6]) 
-    plt.imshow(min_norm,cmap=pylab.cm.gray) #Alpha para visualización de la máscara generada sobre la imagen original.
+    plt.imshow(min_norm,cmap=pylab.cm.gray)
     plt.title('IMIMPOSEMIN en NORMALIZADA')
 
     return img_ws1, img_ws2
